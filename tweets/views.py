094b249eef010fcf4cbbb0ccc6afcc4161d62394
samarthtambad <tambad.samarth@gmail.com>
2020-10-15 12:47:36 -0400

    add tweet view

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detailed_view(request, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+        # "image_path": obj.image,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
