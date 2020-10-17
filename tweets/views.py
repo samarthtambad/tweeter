bb6ac99e7721066bad4a4714a56f3d35b54271c2
samarthtambad <tambad.samarth@gmail.com>
2020-10-16 21:36:06 -0400

    add send tweet feature

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,8 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
@@ -8,13 +10,26 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
-def tweet_list_view(request, *args, **kwargs):
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    print("post data is", request.POST)
+    next_url = request.POST.get("next") or None
+    print("next_url", next_url)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if next_url:
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+def tweet_list_view(request, ):
     """
     REST API VIEW
     return json response with a list of all tweets
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 999)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
