3da4e5c285a2251506851c943f5f090ad5a9f93a
samarthtambad <tambad.samarth@gmail.com>
2020-10-29 13:04:21 -0400

    change root for react component

--- tweeter-web/src/index.js
+++ tweeter-web/src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetsComponent} from './tweets';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appEl = document.getElementById('root')
+if (appEl) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appEl
+  );
+}
+
+const tweetEl = document.getElementById("tweeter")
+if (tweetEl) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <TweetsComponent />
+    </React.StrictMode>,
+    tweetEl
+  );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
