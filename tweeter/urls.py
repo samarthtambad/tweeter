7b2150b19980bccd9ffca2eab57a46d0c725cc78
samarthtambad <tambad.samarth@gmail.com>
2020-10-29 13:26:03 -0400

    render react app from django

--- tweeter/urls.py
+++ tweeter/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from tweets.views import (
     home_view, 
@@ -28,8 +31,12 @@ from tweets.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('react/', TemplateView.as_view(template_name='react.html')),
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detailed_view),
     path('api/tweets/', include('tweets.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
